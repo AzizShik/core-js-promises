b57561e3e4ae76855e1c54df7477ffd09ecb767b
Abdyl-Aziz <azizshihabov@mail.ru>
2026-01-26 19:55:51 +0300

    feat: add solution for getPromiseResult task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -19,7 +19,7 @@
  */
 function getPromise(number) {
   return new Promise((resolve, reject) => {
-    if (number > 0) {
+    if (number >= 0) {
       resolve(number);
     } else {
       reject();
@@ -39,8 +39,8 @@ function getPromise(number) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source.then(() => 'success').catch(() => 'fail');
 }
 
 /**
